4b06fce02efecd3505237750a508393e3c28ddc0
Juyong Kim <jukim@linkedin.com>
2023-04-15 20:59:32 -0700

    Project skeleton
    XCode gitignore file + Project skeleton

---
+++ notifly-ios-sdk/notifly-ios-sdk/notifly_ios_sdk.h
@@ -0,0 +1,18 @@
+//
+//  notifly_ios_sdk.h
+//  notifly-ios-sdk
+//
+//  Created by Juyong Kim on 4/15/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for notifly_ios_sdk.
+FOUNDATION_EXPORT double notifly_ios_sdkVersionNumber;
+
+//! Project version string for notifly_ios_sdk.
+FOUNDATION_EXPORT const unsigned char notifly_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <notifly_ios_sdk/PublicHeader.h>
+
+
