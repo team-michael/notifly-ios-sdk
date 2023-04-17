dbde3fdc377ba5581fc78d48f454fd384390d8dd
Juyong Kim <jukim@linkedin.com>
2023-04-16 23:04:55 -0700

    NotiflyTestApp Skeleton
    Also moved gitignore file to the project root

---
+++ NotiflyTestApp/NotiflyTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotiflyTestApp
+//
+//  Created by Juyong Kim on 4/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
