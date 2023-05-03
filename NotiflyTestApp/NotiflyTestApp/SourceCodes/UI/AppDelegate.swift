6a2718bf72ef713d6eafad308823de22b2a52a37
edenkim00 <eotjd0986@kaist.ac.kr>
2023-05-03 16:38:47 +0900

    setup environment for testing

--- NotiflyTestApp/NotiflyTestApp/SourceCodes/UI/AppDelegate.swift
+++ NotiflyTestApp/NotiflyTestApp/SourceCodes/UI/AppDelegate.swift
@@ -6,6 +6,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // TODO: remove this code after testing. this section is only for testing.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+           if let error = error {
+                // 권한 요청 중 에러가 발생한 경우 처리
+                print("Failed to request authorization: \(error)")
+            } else {
+                // 권한 요청이 성공한 경우 처리
+                print("Authorization granted: \(granted)")
+            }
+        }
         return true
     }
     
@@ -21,4 +31,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                             didFailToRegisterForRemoteNotificationsWithError: error)
     }
 }
-
