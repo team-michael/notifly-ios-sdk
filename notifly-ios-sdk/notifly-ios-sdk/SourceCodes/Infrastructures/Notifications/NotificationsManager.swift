086d12cf3c7598e444d30b9cc60a434006e1cfa1
edenkim00 <eotjd0986@kaist.ac.kr>
2023-05-04 18:12:54 +0900

    add default UNNotificationPresentationOptions

--- notifly-ios-sdk/notifly-ios-sdk/SourceCodes/Infrastructures/Notifications/NotificationsManager.swift
+++ notifly-ios-sdk/notifly-ios-sdk/SourceCodes/Infrastructures/Notifications/NotificationsManager.swift
@@ -159,7 +159,7 @@ extension NotificationsManager: UNUserNotificationCenterDelegate {
                                        willPresent notification: UNNotification,
                                        withCompletionHandler completion: (UNNotificationPresentationOptions) -> Void) {
         handleNotifcation(notification) {
-            completion(UNNotificationPresentationOptions())
+            completion([.banner, .badge, .sound])
         }
     }
 }
