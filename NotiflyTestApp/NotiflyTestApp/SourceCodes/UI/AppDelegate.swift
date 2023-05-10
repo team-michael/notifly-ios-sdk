7f1b843b8db40a3633a7d37851c25d532befbadf
edenkim00 <eotjd0986@kaist.ac.kr>
2023-05-10 14:53:44 +0900

    fix

--- NotiflyTestApp/NotiflyTestApp/SourceCodes/UI/AppDelegate.swift
+++ NotiflyTestApp/NotiflyTestApp/SourceCodes/UI/AppDelegate.swift
@@ -3,31 +3,35 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
         // TODO: remove this code after testing. this section is only for testing.
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
-           if let error = error {
+            if let error = error {
                 print("Failed to request authorization: \(error)")
             } else {
                 print("Authorization granted: \(granted)")
                 DispatchQueue.main.async {
-                        UIApplication.shared.registerForRemoteNotifications()
+                    UIApplication.shared.registerForRemoteNotifications()
                 }
             }
         }
+
+        Notifly.initialize(launchOptions, projectID: "b80c3f0e2fbd5eb986df4f1d32ea2871", username: "minyong", password: "000000", useCustomClickHandler: false)
         return true
     }
-    
+
     func application(_ application: UIApplication,
-                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data)
+    {
         Notifly.application(application,
                             didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
     }
-    
+
     func application(_ application: UIApplication,
-                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
+                     didFailToRegisterForRemoteNotificationsWithError error: Error)
+    {
         Notifly.application(application,
                             didFailToRegisterForRemoteNotificationsWithError: error)
     }
--- notifly-ios-sdk/notifly-ios-sdk/SourceCodes/Notifly+PublicAPI.swift
+++ notifly-ios-sdk/notifly-ios-sdk/SourceCodes/Notifly+PublicAPI.swift
@@ -1,98 +1,111 @@
+import Combine
 import FirebaseCore
 import FirebaseMessaging
 import Foundation
-import Combine
 import UIKit
 
 /**
  Contains all available Notifly SDK Public APIs.
  */
 public extension Notifly {
-    
     // MARK: - Required Setup API configurations
-    
+
     /**
      Initializes the Notifly SDK. This method is to be called as soon as the app laucnhes. (AppDelegate.applicationDidFinishLaunching)
      */
-    static func initialize(projectID: String,
-                           username: String,
-                           password: String,
-                           useCustomClickHandler: Bool) {
+    static func initialize(
+        _ launchOptions: [UIApplication.LaunchOptionsKey: Any]?,
+        projectID: String,
+        username: String,
+        password: String,
+        useCustomClickHandler: Bool = false
+    ) {
         FirebaseApp.configure() // TODO: Uncomment this once Firebase is configured properly for the project.
-        
-        main = Notifly(projectID: projectID,
-                       username: username,
-                       password: password,
-                       useCustomClickHandler: useCustomClickHandler)
-        
+
+        main = Notifly(
+            launchOptions: launchOptions,
+            projectID: projectID,
+            username: username,
+            password: password,
+            useCustomClickHandler: useCustomClickHandler
+        )
+
         Messaging.messaging().token { token, error in
             if let token = token,
-               error == nil {
+               error == nil
+            {
                 main.notificationsManager.apnDeviceTokenPromise?(.success(token))
             }
         }
-        
+
         if let externalUserID = Globals.externalUserIdInUserDefaults {
             main.userManager.externalUserID = externalUserID
         }
 
         Notifly.main.trackingManager.trackInternalEvent(name: TrackingConstant.Internal.sessionStartEventName, params: nil)
     }
-    
+
     static func application(_ application: UIApplication,
-                            didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+                            didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data)
+    {
         main.notificationsManager.application(application,
                                               didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
     }
-    
+
     static func application(_ application: UIApplication,
-                            didFailToRegisterForRemoteNotificationsWithError error: Error) {
+                            didFailToRegisterForRemoteNotificationsWithError error: Error)
+    {
         main.notificationsManager.application(application,
                                               didFailToRegisterForRemoteNotificationsWithError: error)
     }
-    
+
     // MARK: Optional Setup API configurations.
+
     // - Only use below APIs if your app implements custom push notification handler or passed `false` for `useCustomClickHandler` in the `initialize` method.
-    
+
     static func userNotificationCenter(_ notificationCenter: UNUserNotificationCenter,
                                        didReceive response: UNNotificationResponse,
-                                       withCompletionHandler completion: () -> Void) {
+                                       withCompletionHandler completion: () -> Void)
+    {
         main.notificationsManager.userNotificationCenter(notificationCenter,
                                                          didReceive: response,
                                                          withCompletionHandler: completion)
     }
-    
+
     static func userNotificationCenter(_ notificationCenter: UNUserNotificationCenter,
                                        willPresent notification: UNNotification,
-                                       withCompletionHandler completion: (UNNotificationPresentationOptions) -> Void) {
+                                       withCompletionHandler completion: (UNNotificationPresentationOptions) -> Void)
+    {
         main.notificationsManager.userNotificationCenter(notificationCenter,
                                                          willPresent: notification,
                                                          withCompletionHandler: completion)
     }
-    
+
     // MARK: - On-demand APIs
-    
+
     static func trackEvent(name: String,
                            params: [String: Any]?,
-                           segmentationEventParamKeys: [String]?) {
+                           segmentationEventParamKeys: [String]?)
+    {
         main.trackingManager.track(eventName: name,
                                    isInternal: false,
                                    params: params,
                                    segmentationEventParamKeys: segmentationEventParamKeys)
     }
-    
+
     static func setUserID(_ userID: String?) throws {
         try main.userManager.setExternalUserID(userID)
     }
-    
+
     static func setUserProperties(_ params: [String: Any]) throws {
         try main.userManager.setUserProperties(params)
     }
-    
+
     static func schedulePushNotification(title: String?,
                                          body: String?,
                                          url: URL,
-                                         delay: TimeInterval) {
+                                         delay: TimeInterval)
+    {
         main.notificationsManager.schedulePushNotification(title: title,
                                                            body: body,
                                                            url: url,
