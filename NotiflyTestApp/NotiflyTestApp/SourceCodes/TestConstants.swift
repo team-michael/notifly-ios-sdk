8ebbbeb470dc1444b68d72741f3c4c2b1e5d8b9e
edenkim00 <eotjd0986@kaist.ac.kr>
2023-05-16 19:07:45 +0900

    fix

--- NotiflyTestApp/NotiflyTestApp/SourceCodes/TestConstants.swift
+++ NotiflyTestApp/NotiflyTestApp/SourceCodes/TestConstants.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum TestConstant {
     
-    static let username = "minyong"
+    static let username = "minyong2"
     static let password = "000000"
     static let projectID = "b80c3f0e2fbd5eb986df4f1d32ea2871"
 }
