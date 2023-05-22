cc0277bf9401fcf6389d5d63ab674c09d29b5539
edenkim00 <eotjd0986@kaist.ac.kr>
2023-05-22 14:38:09 +0900

    fix

--- notifly-ios-sdk/notifly-ios-sdk/SourceCodes/Infrastructures/Notifications/WebViewModalViewController.swift
+++ notifly-ios-sdk/notifly-ios-sdk/SourceCodes/Infrastructures/Notifications/WebViewModalViewController.swift
@@ -137,6 +137,12 @@ class WebViewModalViewController: UIViewController, WKNavigationDelegate, WKScri
             case "hide_in_app_message":
                 notifly.trackingManager.trackInternalEvent(eventName: TrackingConstant.Internal.inAppMessageDontShowAgainButtonClicked, eventParams: params)
                 dismissCTATapped()
+                if let modalProps = modalProps as? [String: Any],
+                   let templateName = modalProps["template_name"] as? String,
+                   let newProperty = "hide_in_app_message_" + templateName as? String
+                {
+                    try? Notifly.main.userManager.setUserProperties([newProperty: true])
+                }
             case "survey_submit_button":
                 notifly.trackingManager.trackInternalEvent(eventName: TrackingConstant.Internal.inAppMessageSurveySubmitButtonClicked, eventParams: params)
                 dismissCTATapped()
