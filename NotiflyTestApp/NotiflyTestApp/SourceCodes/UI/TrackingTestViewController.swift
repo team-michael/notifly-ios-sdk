f1ae3df924dbb11bd921aa63425552c4583660cc
edenkim00 <eotjd0986@kaist.ac.kr>
2024-03-13 16:08:19 +0900

    feat: validation if the modal is alreday appeared

--- NotiflyTestApp/NotiflyTestApp/SourceCodes/UI/TrackingTestViewController.swift
+++ NotiflyTestApp/NotiflyTestApp/SourceCodes/UI/TrackingTestViewController.swift
@@ -139,3 +139,26 @@ extension TrackingTestViewController: KeyValueDataInputViewControllerDelegate {
         customEventParams = keyPairs
     }
 }
+
+
+private extension UIWindow {
+    var topMostViewController: UIViewController? {
+        return rootViewController?.topMostViewController
+    }
+}
+
+private extension UIViewController {
+    var topMostViewController: UIViewController {
+        if let presented = presentedViewController {
+            return presented.topMostViewController
+        }
+        if let nav = self as? UINavigationController {
+            return nav.visibleViewController?.topMostViewController ?? nav
+        }
+        if let tab = self as? UITabBarController {
+            return (tab.selectedViewController ?? self).topMostViewController
+        }
+        return self
+    }
+}
+
