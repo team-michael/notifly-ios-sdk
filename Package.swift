da627df8144fb7714652b95f4a0be1ae50a88c21
edenkim00 <eotjd0986@kaist.ac.kr>
2024-06-26 18:04:06 +0900

    chore: setup swift package manager for push extension

--- Package.swift
+++ Package.swift
@@ -4,26 +4,35 @@ import PackageDescription
 let package = Package(
     name: "notifly_sdk",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v13)
     ],
     products: [
         .library(
             name: "notifly_sdk",
             targets: ["notifly_sdk"]
         ),
+        .library(
+            name: "notifly_sdk_push_extension",
+            targets: ["notifly_sdk_push_extension"]
+        )
     ],
     dependencies: [
         .package(name: "Firebase",
-                 url: "https://github.com/firebase/firebase-ios-sdk.git", "8.0.0"..."11.0.0"),
+                 url: "https://github.com/firebase/firebase-ios-sdk.git", "8.0.0" ... "11.0.0")
     ],
     targets: [
         .target(
             name: "notifly_sdk",
             dependencies: [
-                .product(name: "FirebaseMessaging", package: "Firebase"),
+                .product(name: "FirebaseMessaging", package: "Firebase")
             ],
             path: "Sources/Notifly/notifly-ios-sdk/notifly-ios-sdk",
             sources: ["SourceCodes", "PrivacyInfo.xcprivacy"]
         ),
+        .target(
+            name: "notifly_sdk_push_extension",
+            path: "Sources/Notifly/notifly-ios-sdk/notifly-ios-sdk",
+            sources: ["SourceCodes/NotiflyExtension", "SourceCodes/NotiflyUtil", "PrivacyInfo.xcprivacy"]
+        )
     ]
 )
