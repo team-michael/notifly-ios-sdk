251dd5bc1f6687c47bb75ba935aa7c2081009b94
minyonglee <rickylee87@gmail.com>
2025-11-13 11:02:07 +0900

    fix: in app popup transparent background

--- Sources/Notifly/notifly-ios-sdk/notifly-ios-sdk/SourceCodes/NotiflyUtil/Constants.swift
+++ Sources/Notifly/notifly-ios-sdk/notifly-ios-sdk/SourceCodes/NotiflyUtil/Constants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum NotiflySdkConfig {
-    static let sdkVersion: String = "1.17.3"
+    static let sdkVersion: String = "2.1.0"
     static var sdkWrapperVersion: String?
     static let sdkType: String = "native"
     static var sdkWrapperType: SdkWrapperType?
