de396d3b5a6a33eff9e21bb7271925cb70d63724
Andrej Karpathy <team@greyboxhq.com>
2026-06-17 05:52:24 +0000

    chore: bump FCM refresh beta version

--- Sources/Notifly/notifly-ios-sdk/notifly-ios-sdk/SourceCodes/NotiflyUtil/Constants.swift
+++ Sources/Notifly/notifly-ios-sdk/notifly-ios-sdk/SourceCodes/NotiflyUtil/Constants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum NotiflySdkConfig {
-    static let sdkVersion: String = "2.6.1-beta.0"
+    static let sdkVersion: String = "2.6.1-beta.1"
     static var sdkWrapperVersion: String?
     static let sdkType: String = "native"
     static var sdkWrapperType: SdkWrapperType?
